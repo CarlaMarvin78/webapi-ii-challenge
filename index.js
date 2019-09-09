67be1cc7d90d56a01830c262342631716adf3b90
Carla Marvin <carlamarvin78@gmail.com>
2019-09-09 10:31:16 -0400

    Updated for Heroku deployment

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const express = require('express');
 const server = express();
 const postsRouter = require ('./postsRouter');
+require('dotenv').config();
 
 server.use (express.json());
 server.use ('/api/posts', postsRouter);
+const port = process.env.PORT || 5000;
 
-server.listen(5000, () => console.log("server on 5000"));
+server.listen(port, () => console.log(`server on ${port}`));
